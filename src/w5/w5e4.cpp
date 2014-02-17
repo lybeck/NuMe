e379ac6cdf7cdf23372f94d468bf0c40f5f89d36
Lasse Lybeck <lasse.lybeck@helsinki.fi>
2014-02-17 11:55:20 +0200

    Refactored w5e4

--- src/w5/w5e4.cpp
+++ src/w5/w5e4.cpp
@@ -16,38 +16,31 @@ using namespace std;
 
 typedef double (*func) (double);
 
-double der2(func f, double h, double x0) {
-    Vec_DP y(6), dy(6), ddy(6);
-    int i;
-    for (i = 1; i <= 5; i++) {
-        y[i] = f(x0 + (i - 3) * h);
-    }
-    numder(y, dy, h, 5);
-    numder(dy, ddy, h, 5);
-    return ddy[3];
-}
-
 void draw_2nd_der(func f, double minx, double maxx) {
     double h = 1e-5;
+    double a = minx - 2 * h;
+    double b = maxx + 2 * h;
     int points = 200;
     double dx = (maxx - minx) / (points - 1);
 
     myplot::plot_data function_points("b-2"), der2_points("r-");
     for (double x = minx; x <= maxx; x += dx) {
         function_points.add_point(x, f(x));
-        der2_points.add_point(x, der2(f, h, x));
+        der2_points.add_point(x, numdf(f, a, b, x, h, 2));
     }
     myplot::plot({function_points, der2_points});
 }
 
 void draw_2nd_der_sum(func f, double minx, double maxx) {
     double h = 1e-5;
+    double a = minx - 2 * h;
+    double b = maxx + 2 * h;
     int points = 200;
     double dx = (maxx - minx) / (points - 1);
 
     myplot::plot_data function_points("b-2");
     for (double x = minx; x <= maxx; x += dx) {
-        function_points.add_point(x, f(x) + der2(f, h, x));
+        function_points.add_point(x, f(x) + numdf(f, a, b, x, h, 2));
     }
     myplot::plot({function_points});
 }
